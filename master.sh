ce7d90acf1143dece830eed56a003c1dd821b560
AniLuc <143954427+AniLuc@users.noreply.github.com>
2024-04-16 17:40:32 +0500

    Create master.sh

---
+++ master.sh
@@ -0,0 +1,16 @@
+IPADDR="192.168.80.211"
+NODENAME=$(hostname -s)
+POD_CIDR="192.168.0.0/16"
+
+sudo kubeadm init --apiserver-advertise-address=$IPADDR  --apiserver-cert-extra-sans=$IPADDR  --pod-network-cidr=$POD_CIDR --node-name $NODENAME --ignore-preflight-errors Swap
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl get po -n kube-system
+
+kubectl get --raw='/readyz?verbose'
+
+kubectl cluster-info 
+
